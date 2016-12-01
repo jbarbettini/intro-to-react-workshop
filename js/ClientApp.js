231becc22072740a2461f9e8c7a490a4b22aea10
Jennifer Ong <jenn.r.ong@gmail.com>
2016-12-01 09:17:12 -0800

    Linted files after webpack

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import MyTitle from './MyTitle'
 
-var div = React.DOM.div 
+var div = React.DOM.div
 
 var MyTitleFactory = React.createFactory(MyTitle)
 
