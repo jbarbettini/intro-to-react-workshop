106ae0589df255cd2b1670b7b730333abed3e6b1
Jennifer Ong <jenn.r.ong@gmail.com>
2016-12-01 08:54:44 -0800

    Add standard and run script for linting

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,8 +1,10 @@
-var div = React.DOM.div;
-var h1 = React.DOM.h1; 
+/* global React ReactDOM */
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
-  render: function() {
+  render: function () {
     return (
       div(null, h1({ style: {color: this.props.color} }, this.props.title))
     )
@@ -12,17 +14,17 @@ var MyTitle = React.createClass({
 var MyTitleFactory = React.createFactory(MyTitle)
 
 var MyFirstComponent = React.createClass({
-      render: function() {
-        return (
+  render: function () {
+    return (
           div(null, [
             MyTitleFactory({title: 'Props are great!', color: 'rebeccapurple'}),
             MyTitleFactory({title: 'Props are great!', color: 'dodgerblue'}),
             MyTitleFactory({title: 'Use props everywhere!', color: 'mediumaquamarine'}),
             MyTitleFactory({title: 'Props are the best!', color: 'peru'})
-            ]
+          ]
           )
-        )
-      }
-    })
+    )
+  }
+})
 
 ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
